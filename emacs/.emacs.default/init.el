db01ad6bd8c0414b5dd698a4431e6c39ae72f4ab
Denis Kipchakbaev <denis.kipchakbaev@weekendesk.fr>
2021-05-23 22:48:52 +0200

    Switch to zenburn theme

--- emacs/.emacs.default/init.el
+++ emacs/.emacs.default/init.el
@@ -158,12 +158,21 @@
 
 ;; A bunch of old-school color themes
 ;; Wanted to check `gnome2' theme
-(use-package color-theme-modern)
-(use-package humanoid-themes)
-(use-package dracula-theme)
+;; (use-package color-theme-modern)
+;; (use-package humanoid-themes)
+;; (use-package dracula-theme)
 ;; 't' avoids prompting for loading theme each time
 ;; (load-theme 'dracula t)
-(load-theme 'humanoid-dark t)
+(use-package zenburn-theme
+  :config
+  ;; use variable-pitch fonts for some headings and titles
+  (setq zenburn-use-variable-pitch t)
+  ;; scale headings in org-mode
+  (setq zenburn-scale-org-headlines t)
+  ;; scale headings in outline-mode
+  (setq zenburn-scale-outline-headlines t)
+  )
+(load-theme 'zenburn t)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
