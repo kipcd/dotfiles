40455dbd738c1f03bbc6d75ed7e35923fea00412
deniswed <denis.kipchakbaev@weekendesk.fr>
2021-11-12 21:30:13 +0100

    autorandr and screen layout scripts

---
+++ X/.config/.screenlayout/xrandr-dual-dp-1680.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI1 --off --output VIRTUAL1 --off --output HDMI-1-0 --off --output DP-1-0 --off --output DP-1-1 --primary --mode 1680x1050 --pos 1920x0 --rotate normal
