7da419dcd561d5a727914f8fd817175122f4821c
deniswed <denis.kipchakbaev@weekendesk.fr>
2021-11-13 09:42:02 +0100

    Use the same bar for both monitors
    However have some problems with app tray icons, Slack icons sometimes
    doesn't appear

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,12 +9,22 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar --reload mainbar 2>$HOME/logs/polybar-mainbar.log &
 
 # load external bar in every non-primary monitor
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep --invert-match "primary" | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload external 2>$HOME/logs/polybar-external-$m.log &
+#   done
+# else
+#   polybar --reload external 2>$HOME/logs/polybar-external.log &
+# fi
+
+
+# Just use mainbar on each non-primary monitor
 if type "xrandr"; then
   for m in $(xrandr --query | grep --invert-match "primary" | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload external 2>$HOME/logs/polybar-external-$m.log &
+    MONITOR=$m polybar --reload mainbar 2>$HOME/logs/polybar-external-$m.log &
   done
 else
-  polybar --reload external 2>$HOME/logs/polybar-external.log &
+  polybar --reload mainbar 2>$HOME/logs/polybar-external.log &
 fi
 
 echo "Polybar launched..."
